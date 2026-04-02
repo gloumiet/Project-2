4d6ece036d72c8d7c570c242ad95b8cca8154b59
gloumiet <gmloumiet@gmail.com>
2026-04-02 10:23:25 -0500

    fixed issues and added graident background and bubbles

--- js/main.js
+++ js/main.js
@@ -1,180 +1,197 @@
-// Bubbles
-const container = document.getElementById('bubble-container');
-function createBubble() {
-  const bubble = document.createElement('div');
-  bubble.classList.add('bubble');
+// BUBBLES
+const container = document.getElementById("bubble-container");
 
-  const size = Math.random() * 30 + 10;
-  bubble.style.width = `${size}px`;
-  bubble.style.height = `${size}px`;
-  bubble.style.left = `${Math.random() * 100}vw`;
-  bubble.style.animationDuration = `${Math.random() * 2 + 3}s`;
+for(let i = 0; i < 25; i++){
 
-  container.appendChild(bubble);
-  bubble.addEventListener('animationend', () => bubble.remove());
+let bubble = document.createElement("div");
+
+bubble.classList.add("bubble");
+
+let size = Math.random()*40 + 10;
+
+bubble.style.width = size + "px";
+bubble.style.height = size + "px";
+
+bubble.style.left = Math.random()*100 + "%";
+
+bubble.style.animationDuration = (Math.random()*10 + 10) + "s";
+
+container.appendChild(bubble);
+
 }
-setInterval(createBubble, 300);
 
 
-
-
-
-
-
-// Flavors
-const flavors = [
-  {
-    name: "Cherry Bomb",
-    description: "A refreshing berry blend to energize your day!",
-    image: "img/CherryBomb.webp",
-    gradient: "linear-gradient(135deg, #9e152e, #f5abb7)"
-  },
-  {
-    name: "Cherry Slush",
-    description: "Explosive cherry flavor with a sweet kick!",
-    image: "img/CherrySlush.webp",
-    gradient: "linear-gradient(135deg, #ff6a6a, #ffb6b9)"
-  },
-  {
-    name: "Cherry Twist",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/CherryTwist.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Strawberry Sunrise",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/StrawberrySunrise.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Sherbert Swirl",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/SherbertSwirl.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Orange Kiss",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/OrangeKiss.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Pink Slush",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/PinkSlush.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Cotton Candy",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/CottonCandy.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Lime Slush",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/LimeSlush.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Watermelon Wave",
-    description: "Light, dreamy, and refreshing — just float away!",
-    image: "img/WatermelonWave.webp",
-    gradient: "linear-gradient(135deg, #ffb347, #ffcc33)"
-  },
-  {
-    name: "Juicy Peach",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/JuicyPeach.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Blue Slush",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/BlueSlush.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Dream Float",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/DreamFloat.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Breezeberry",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/Breezeberry.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },
-  {
-    name: "Cosmic Stardust",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/CosmicStardust.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  },{
-    name: "Hawaiian Shaved Ice",
-    description: "A cosmic mix of fruity flavors for ultimate energy!",
-    image: "img/HawaiianShavedIce.webp",
-    gradient: "linear-gradient(135deg, #8e44ad, #3498db)"
-  }
-];
-
-
-
-
-
+// HERO + FLAVORS
 const heroSection = document.querySelector('.hero');
 const flavorName = document.querySelector('.flavor-name');
 const flavorDescription = document.querySelector('.flavor-description');
 const productImage = document.getElementById('product-image');
 
 
+// FLAVORS
+const flavors = [
 
+{
+name: "Cherry Bomb",
+description: "A refreshing berry blend to energize your day!",
+image: "img/Cherry Bomb.webp",
+gradient: "linear-gradient(135deg, #f4bfdb, #300813)"
+},
 
+{
+name: "Cherry Slush",
+description: "Explosive cherry flavor with a sweet kick!",
+image: "img/Cherry Slush.webp",
+gradient: "linear-gradient(135deg, #ffd9d6, #e53123)"
+},
 
-let currentFlavor = 0;
+{
+name: "Cherry Twist",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Cherry Twist.webp",
+gradient: "linear-gradient(135deg, #eb456a, #e3ec5a)"
+},
 
-function updateFlavor(index) {
-  const flavor = flavors[index];
+{
+name: "Strawberry Sunrise",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Strawberry Sunrise.webp",
+gradient: "linear-gradient(135deg, #ff6b90, #ffdcb3)"
+},
 
-  // Fade out
-  productImage.style.opacity = 0;
-  flavorName.style.opacity = 0;
-  flavorDescription.style.opacity = 0;
+{
+name: "Sherbert Swirl",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Sherbert Swirl.webp",
+gradient: "linear-gradient(135deg, #ffba41, #3bb1a2)"
+},
 
-  setTimeout(() => {
-    productImage.src = flavor.image;
-    heroSection.style.background = flavor.gradient;
-    flavorName.textContent = flavor.name;
-    flavorDescription.textContent = flavor.description;
+{
+name: "Orange Kiss",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Orange Kiss.webp",
+gradient: "linear-gradient(135deg, #ffce2f, #ff5d9f)"
+},
 
+{
+name: "Pink Slush",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Pink Slush.webp",
+gradient: "linear-gradient(135deg, #f7e6ee, #ea37a0)"
+},
 
-    // Fade in
-    productImage.style.opacity = 1;
-    flavorName.style.opacity = 1;
-    flavorDescription.style.opacity = 1;
-  }, 400);
+{
+name: "Cotton Candy",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Cotton Candy.webp",
+gradient: "linear-gradient(135deg, #f1b0e4, #a3f0e8)"
+},
+
+{
+name: "Lime Slush",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Lime Slush.webp",
+gradient: "linear-gradient(135deg, #dfedd4, #7dcc3c)"
+},
+
+{
+name: "Watermelon Wave",
+description: "Light, dreamy, and refreshing — just float away!",
+image: "img/Watermelon Wave.webp",
+gradient: "linear-gradient(135deg, #b5ec63, #68eeec)"
+},
+
+{
+name: "Juicy Peach",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Juicy Peach.webp",
+gradient: "linear-gradient(135deg, #e65f42, #3bb1a2)"
+},
+
+{
+name: "Blue Slush",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Blue Slush.webp",
+gradient: "linear-gradient(135deg, #cff6ff, #6be5ff)"
+},
+
+{
+name: "Dream Float",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Dream Float.webp",
+gradient: "linear-gradient(135deg, #f2b687, #2e638f)"
+},
+
+{
+name: "Breezeberry",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Breezeberry.webp",
+gradient: "linear-gradient(135deg, #9eecfc, #1a243e)"
+},
+
+{
+name: "Cosmic Stardust",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Cosmic Stardust.webp",
+gradient: "linear-gradient(135deg, #ad99d6, #2c0d56)"
+},
+
+{
+name: "Hawaiian Shaved Ice",
+description: "A cosmic mix of fruity flavors for ultimate energy!",
+image: "img/Hawaiian Shaved Ice.webp",
+gradient: "linear-gradient(135deg, #ff1756, #6c25bf)"
 }
 
-// Arrow buttons
-document.getElementById('next-btn').addEventListener('click', () => {
-  currentFlavor = (currentFlavor + 1) % flavors.length;
-  updateFlavor(currentFlavor);
-});
+];
 
-document.getElementById('prev-btn').addEventListener('click', () => {
-  currentFlavor = (currentFlavor - 1 + flavors.length) % flavors.length;
-  updateFlavor(currentFlavor);
-});
 
-// Initialize
+// CURRENT FLAVOR
+let currentFlavor = 0;
+
+
+function updateFlavor(index){
+
+const flavor = flavors[index];
+
+// Fade out
+productImage.style.opacity = 0;
+flavorName.style.opacity = 0;
+flavorDescription.style.opacity = 0;
+
+setTimeout(()=>{
+
+productImage.src = flavor.image;
+heroSection.style.backgroundImage = flavor.gradient;
+flavorName.textContent = flavor.name;
+flavorDescription.textContent = flavor.description;
+
+// Fade in
+productImage.style.opacity = 1;
+flavorName.style.opacity = 1;
+flavorDescription.style.opacity = 1;
+
+},400);
+
+}
+
+
+// ARROWS
+document.getElementById('next-btn').addEventListener('click', ()=>{
+
+currentFlavor = (currentFlavor + 1) % flavors.length;
 updateFlavor(currentFlavor);
 
+});
 
 
+document.getElementById('prev-btn').addEventListener('click', ()=>{
+
+currentFlavor = (currentFlavor - 1 + flavors.length) % flavors.length;
+updateFlavor(currentFlavor);
+
+});
 
 
-
-
-
+// START
+updateFlavor(currentFlavor);
